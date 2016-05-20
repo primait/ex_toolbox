8a049eb638f1436c09fb09c458d0d31ec9f05ea0
Matteo Giachino <matteo.giachino@prima.it>
2016-05-20 14:11:59 +0200

    "new version 0.0.2"

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExPrimaToolbox.Mixfile do
 
   def project do
     [app: :ex_prima_toolbox,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
